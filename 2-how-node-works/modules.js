fd88ae31e35accbf8d057ce939119a420eb01f83
shivaniJEunimart <shivani.jaiswal@eunimart.com>
2020-08-20 23:57:08 +0530

    added caching

--- 2-how-node-works/modules.js
+++ 2-how-node-works/modules.js
@@ -6,6 +6,15 @@ const C = require("./test-module-1");
 const calc1 = new C();
 console.log(calc1.add(2, 3));
 
-//export
+//exports
 const calc2 = require("./test-module-2");
 console.log(calc2.add(2, 3));
+
+//different way of exporting
+const { add, multiply } = require("./test-module-2");
+console.log(multiply(2, 5));
+
+//caching
+require("./test-module-3")();
+require("./test-module-3")();
+require("./test-module-3")();
