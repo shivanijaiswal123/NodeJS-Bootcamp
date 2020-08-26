ebf9c1b5ce307f5e0c5d67473f8ef1f0ff3e376f
shivaniJEunimart <shivani.jaiswal@eunimart.com>
2020-08-27 00:03:31 +0530

    Promises with Async/Await

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -19,7 +19,25 @@ const writeFilePro = (file, data) => {
   });
 };
 
-readFilePro(`${__dirname}/dog.txt`)
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
+
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    console.log(res.body.message);
+
+    await writeFilePro("dog-img.txt", res.body.message);
+    console.log("Random dog image saved to file!");
+  } catch (err) {
+    console.log(err);
+  }
+};
+getDogPic();
+
+/*readFilePro(`${__dirname}/dog.txt`)
   .then((data) => {
     console.log(`Breed: ${data}`);
     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
@@ -33,4 +51,4 @@ readFilePro(`${__dirname}/dog.txt`)
   })
   .catch((err) => {
     console.log(err);
-  });
+  });*/
